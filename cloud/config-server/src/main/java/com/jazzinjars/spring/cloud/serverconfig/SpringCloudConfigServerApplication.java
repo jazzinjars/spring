133eb24fe490a46e4f02cf44b90dcdb0eca13219
jazzinjars <hectorardila@gmail.com>
2018-09-10 22:17:14 +0200

    SpringCloud Config Server sample

---
+++ cloud/config-server/src/main/java/com/jazzinjars/spring/cloud/serverconfig/SpringCloudConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.jazzinjars.spring.cloud.serverconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+    }
+}
