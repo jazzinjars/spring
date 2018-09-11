70d85826b67a5e42db3af376fef2b090e380861a
jazzinjars <hectorardila@gmail.com>
2018-09-11 19:20:35 +0200

    Getting Config Files from Git Repo correctly

---
+++ cloud/config-server-git/src/test/java/com/jazzinjars/spring/cloud/configservergit/SpringCloudConfigServerGitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jazzinjars.spring.cloud.configservergit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class SpringCloudConfigServerGitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
