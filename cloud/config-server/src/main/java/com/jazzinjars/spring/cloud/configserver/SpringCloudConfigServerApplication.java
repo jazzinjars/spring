27698f9c8b6d99ace1437ee830d20d64038b8aa1
jazzinjars <hectorardila@gmail.com>
2018-09-11 16:20:05 +0200

    Spring Config server with Git

--- cloud/config-server/src/main/java/com/jazzinjars/spring/cloud/configserver/SpringCloudConfigServerApplication.java
+++ cloud/config-server/src/main/java/com/jazzinjars/spring/cloud/configserver/SpringCloudConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.jazzinjars.spring.cloud.serverconfig;
+package com.jazzinjars.spring.cloud.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
